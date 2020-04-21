90b30571c4c7de825534e7d62d93795424319d6d
Stephan Stan <stephanstan@gmail.com>
2020-04-20 22:13:11 -0600

    minute 35:36 of https://www.youtube.com/watch?v=vtPkZShrvXQ&t=188s

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,10 +3,9 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -21,5 +20,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         PersonService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return PersonService.getAllPeople();
     }
 }
